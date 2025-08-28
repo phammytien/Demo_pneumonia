6b18ce9c9918fa274d63fe73f98bbd789d107be3
phammytien <phmytien19022004@gmail.com>
2025-08-28 18:15:39 +0700

    Update db_utils.py

--- pneumonia_app/utils/db_utils.py
+++ pneumonia_app/utils/db_utils.py
@@ -1,18 +1,19 @@
 import mysql.connector
 from mysql.connector import Error
 
+
 # Hàm tạo kết nối
 def get_connection():
     try:
         conn = mysql.connector.connect(
-            host="sql100.infinityfree.com",    # host MySQL (từ InfinityFree)
-            port=3306,                         # cổng MySQL mặc định
-            database="if0_39810423_XXX",       # thay XXX bằng tên DB cụ thể của bạn
-            user="if0_39810423",               # username MySQL
-            password="JHCUnmjxAbZC"            # password MySQL
+            host="127.0.0.1",       # host MySQL
+            port=3307,              # cổng MySQL
+            database="pneumonia_app_1",  # tên database
+            user="root",            # user MySQL
+            password="123456"       # password MySQL
         )
         if conn.is_connected():
-            print("✅ Đã kết nối MySQL thành công (InfinityFree)!")
+            print("✅ Đã kết nối MySQL thành công!")
             return conn
     except Error as e:
         print("❌ Lỗi khi kết nối MySQL:", e)
